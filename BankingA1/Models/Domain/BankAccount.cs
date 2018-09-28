1b1309a67d7034c39a4760cefd236004319827e0
Stefaan Samyn <stefaan.samyn@hogent.be>
2018-09-28 09:45:27 +0200

    Creëer klasse BankAccount

---
+++ BankingA1/Models/Domain/BankAccount.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace BankingA1.Models.Domain
+{
+    public class BankAccount
+    {
+        // private string _accountNumber;
+        #region Fields
+         private decimal _balance;
+
+        #endregion
+
+        #region Properties
+        public string AccountNumber { get; private set; }
+
+        public decimal Balance {
+            get {
+                return _balance;
+            }
+            private set {
+                if (value < 0)
+                    throw new ArgumentException("No negative balance allowed");
+                _balance = value;
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+        public BankAccount()
+        {
+
+        }
+        public BankAccount(string accountNumber)
+        {
+            AccountNumber = accountNumber;
+        }
+
+        public BankAccount(string accountNumber, decimal balance) : this(accountNumber)
+        {
+            Balance = balance;
+        }
+
+        #endregion
+
+        #region Methods
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+
+        #endregion
+    }
+}
--- BankingA1/Program.cs
+++ BankingA1/Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using BankingA1.Models.Domain;
+using System;
 
 namespace BankingA1
 {
-    class Program
+    public static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Object Initializer
+            //BankAccount anotherBA = new BankAccount()
+            //{
+            //    AccountNumber = "123-12312334-99",
+            //    Balance = 200
+            //};
+
+            BankAccount myBA = new BankAccount("123-12312312-99", 50);
+
+            Console.WriteLine($"Account number is {myBA.AccountNumber}.");
+            Console.WriteLine($"Balance is {myBA.Balance} Euro");
+            myBA.Deposit(200);
+            Console.WriteLine($"Balance is {myBA.Balance} Euro");
+            myBA.Withdraw(50);
+            Console.WriteLine($"Balance is {myBA.Balance} Euro");
+            Console.ReadKey();
         }
     }
 }
